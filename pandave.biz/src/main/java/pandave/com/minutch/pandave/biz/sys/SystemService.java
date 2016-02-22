fbfc40a614943abdccb316f4c37cdc4bcf622086
闵崇建 <chongjian.min@yuanpinzhuyi.com>
2016-02-22 16:39:52 +0800

    1.remove unvalid web module

--- pandave.biz/src/main/java/pandave/com/minutch/pandave/biz/sys/SystemService.java
+++ pandave.biz/src/main/java/pandave/com/minutch/pandave/biz/sys/SystemService.java
@@ -1,6 +1,10 @@
 package pandave.com.minutch.pandave.biz.sys;
+
+import org.springframework.stereotype.Service;
+
 /**
  * Created by Minutch on 16/2/22.
  */
+@Service
 public class SystemService {
 }
