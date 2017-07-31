cbfb35166ee724d9edf220855c9101a63f3ad624
闵崇建 <chongjian.min@yuanpinzhuyi.com>
2017-08-01 01:12:46 +0800

    1.去除MQ和CommonResource

--- fox.biz/src/main/java/com/minutch/fox/mq/topic/TopicConsumer.java
+++ fox.biz/src/main/java/com/minutch/fox/mq/topic/TopicConsumer.java
@@ -1,20 +1,20 @@
 package com.minutch.fox.mq.topic;
 
-import com.minutch.fox.mq.base.MQConstants;
+//import com.minutch.fox.mq.base.MQConstants;
 //import com.yuanpin.common.mq.annotaion.MqConsumer;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Service;
 
 /**
  * Created by Minutch on 16/12/13.
  */
-@Slf4j
-@Service
+//@Slf4j
+//@Service
 //@MqConsumer
 public class TopicConsumer {
 
 //    @MqConsumer(tags = MQConstants.TAGS_TOPIC_SEND)
-    public void sendTopic(String  topic) {
-        System.out.println(topic);
-    }
+//    public void sendTopic(String  topic) {
+//        System.out.println(topic);
+//    }
 }
--- fox.biz/src/main/java/com/minutch/fox/mq/topic/TopicProducer.java
+++ fox.biz/src/main/java/com/minutch/fox/mq/topic/TopicProducer.java
@@ -3,14 +3,14 @@ package com.minutch.fox.mq.topic;
 import com.minutch.fox.mq.base.MQConstants;
 //import com.yuanpin.common.mq.annotaion.MqProducer;
 //import com.yuanpin.common.mq.producer.SqMessage;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Service;
+//import lombok.extern.slf4j.Slf4j;
+//import org.springframework.stereotype.Service;
 
 /**
  * Created by Minutch on 16/12/13.
  */
-@Slf4j
-@Service
+//@Slf4j
+//@Service
 //@MqProducer
 public class TopicProducer {
 
